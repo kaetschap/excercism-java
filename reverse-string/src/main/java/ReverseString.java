4078fa55a5dab3e8f377aa85b7158161e8384afe
sonja <sonja@linux.fritz.box>
2017-12-11 14:18:41 +0100

    added new bare exercise

---
+++ reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,7 @@
+class ReverseString {
+  
+    String reverse(String stringToReverse) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+  
+}
